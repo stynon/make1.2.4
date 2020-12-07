fd744683b7c5236f08dec6e09e050107c5d65c98
PC Stijn <stijn.janssen@student.ucll.be>
2020-12-07 21:33:04 +0100

    Version1.1

---
+++ gpioPin.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+Info about our project comes here.
+"""
+
+# Import
+import os
+
+__author__ = "Stijn Janssen"
+__email__ = "stijn.janssen@student.kdg.be"
+__status__ = "Development"
+
+
+def pinout():
+    os.system("pinout")
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import systemInfo
 import install
 #import networkscan
 import portscan
+import gpioPin
 
 __author__ = "Stijn Janssen"
 __email__ = "stijn.janssen@student.kdg.be"
@@ -31,30 +32,38 @@ def menu():
     1: Ip weergave       | 5: Networkscan               | 9:  Apparmor
     2: PasswordGen       | 6: System update en upgrade  | 10: Computer management
     3: System info       | 7: Portscan
-    4: Install openvpn   | 8: Firewall
-""")
+    4: Install openvpn   | 8: gpio
+    Choose the option you like: """)
 
     if choice == "1":
         ifConfig.ip()
+        print("\n")
         restart()
     elif choice == "2":
         passwordgen.passwordgenerator()
+        print("\n")
         restart()
     elif choice == "3":
         systemInfo.sysinfo()
+        print("\n")
         restart()
     elif choice == "4":
         install.installvpn()
+        print("\n")
         restart()
     elif choice == "5":
         c=2
     elif choice == "6":
         updateSystem.updatesystem()
+        print("\n")
+        restart()
     elif choice == "7":
         portscan.port()
+        print("\n")
         restart()
     elif choice == "8":
-        x = 1
+        gpioPin.pinout()
+        restart()
     elif choice == "9":
         x=1
     elif choice == "10":
