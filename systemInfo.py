8e20d40e41a4001ab253c63511ca1607d0da7e6d
PC Stijn <stijn.janssen@student.ucll.be>
2020-12-07 16:34:55 +0100

    Version1.1

--- systemInfo.py
+++ systemInfo.py
@@ -12,15 +12,9 @@ __status__ = "Development"
 
 
 def sysinfo():
-    print
-    'system   :', platform.system()
-    print
-    'node     :', platform.node()
-    print
-    'release  :', platform.release()
-    print
-    'version  :', platform.version()
-    print
-    'machine  :', platform.machine()
-    print
-    'processor:', platform.processor()
+    print('system   :', platform.system())
+    print('node     :', platform.node())
+    print('release  :', platform.release())
+    print('version  :', platform.version())
+    print('machine  :', platform.machine())
+    print('processor:', platform.processor())
